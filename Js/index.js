7c7fb7a77d0184cee07cc393375ac6fd85d7e16a
MichellePeroni <105086262+MichellePeroni@users.noreply.github.com>
2023-11-07 21:01:42 -0300

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,23 @@
+import {Card} from "./card.js";
 import {NavBar} from "./NavBar.js";
 
-const navBar = new NavBar()
+const navBar = new NavBar();
+const card = new Card();
 navBar.criarNavBar()
+
+const produtos = [
+    {
+        nome: 'pizza frango com catupiry',
+        texto: 'Essa é a melhor pizza do senac',
+        imagem: 'https://www.hojetemfrango.com.br/wp-content/uploads/2019/01/shutterstock_333724454.jpg',
+    },
+    {
+        nome: 'pizza portuguesa',
+        texto: 'Essa é a segunda melhor pizza do senac',
+        imagem: 'https://www.hojetemfrango.com.br/wp-content/uploads/2019/01/shutterstock_333724454.jpg',
+    }
+]
+
+produtos.forEach(produto => {
+    card.criarCard(produto.imagem, produto.nome, produto.texto);
+})
